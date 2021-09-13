277bbc28ef3d35e0ad3262a105e71287e93cb3b1
Gabe Ewusk <gabeewsuk@gmail.com>
2021-09-13 11:04:03 -0600

    fixed User id string issue4

--- Folder/parentFunctions/AddNewUsers/bulkWrite.py
+++ Folder/parentFunctions/AddNewUsers/bulkWrite.py
@@ -23,7 +23,28 @@ def addNewUsers(user_ids):
     for x in user_ids:
         counter+=1
         subset.append(x)
-        if i % 200 == 0:
+        if len(user_ids)>5000:
+            if i % 200 == 0:
+                documents = scrapeUsers(subset)
+                print("Length of documents after API is:"+str(len(documents)))
+                print(str(counter)+" have been updated so far!")
+                with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+                    future_to_url = (executor.submit(findAndUpdateUser, db, document)for document in documents)
+                    time1 = time.time()
+                    for future in concurrent.futures.as_completed(future_to_url):
+                        try:
+                            data = future.result()
+                            #print(data)
+                        except Exception as exc:
+                            print("Exception is:"+str(exc))
+                        finally:
+                            NAN = 0
+                            
+
+                    time2 = time.time()
+                print(f'Took {time2-time1:.2f} s')
+                subset = []
+        if len(user_ids)<20:
             documents = scrapeUsers(subset)
             print("Length of documents after API is:"+str(len(documents)))
             print(str(counter)+" have been updated so far!")
@@ -43,6 +64,75 @@ def addNewUsers(user_ids):
                 time2 = time.time()
             print(f'Took {time2-time1:.2f} s')
             subset = []
+
+
+        if len(user_ids)>20 and len(user_ids)<500:
+            if i % 10 == 0:
+                documents = scrapeUsers(subset)
+                print("Length of documents after API is:"+str(len(documents)))
+                print(str(counter)+" have been updated so far!")
+                with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+                    future_to_url = (executor.submit(findAndUpdateUser, db, document)for document in documents)
+                    time1 = time.time()
+                    for future in concurrent.futures.as_completed(future_to_url):
+                        try:
+                            data = future.result()
+                            #print(data)
+                        except Exception as exc:
+                            print("Exception is:"+str(exc))
+                        finally:
+                            NAN = 0
+                            
+
+                    time2 = time.time()
+                print(f'Took {time2-time1:.2f} s')
+                subset = []
+
+        if len(user_ids)>500 and len(user_ids)<1500:
+            if i % 50 == 0:
+                documents = scrapeUsers(subset)
+                print("Length of documents after API is:"+str(len(documents)))
+                print(str(counter)+" have been updated so far!")
+                with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+                    future_to_url = (executor.submit(findAndUpdateUser, db, document)for document in documents)
+                    time1 = time.time()
+                    for future in concurrent.futures.as_completed(future_to_url):
+                        try:
+                            data = future.result()
+                            #print(data)
+                        except Exception as exc:
+                            print("Exception is:"+str(exc))
+                        finally:
+                            NAN = 0
+                            
+
+                    time2 = time.time()
+                print(f'Took {time2-time1:.2f} s')
+                subset = []
+
+        if len(user_ids)>1500 and len(user_ids)<5000:
+            if i % 10 == 0:
+                documents = scrapeUsers(subset)
+                print("Length of documents after API is:"+str(len(documents)))
+                print(str(counter)+" have been updated so far!")
+                with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+                    future_to_url = (executor.submit(findAndUpdateUser, db, document)for document in documents)
+                    time1 = time.time()
+                    for future in concurrent.futures.as_completed(future_to_url):
+                        try:
+                            data = future.result()
+                            #print(data)
+                        except Exception as exc:
+                            print("Exception is:"+str(exc))
+                        finally:
+                            NAN = 0
+                            
+
+                    time2 = time.time()
+                print(f'Took {time2-time1:.2f} s')
+                subset = []
+
+
         i+=1
 
     print("bulk insert users complete...")
